87e03bc2513827ee5c93733c96d0c40653531a6a
Gang Huang <ghuang@smartehub.serveblog.net>
2014-12-14 10:28:53 -0800

    process input filename as command line parameter

--- csvtabtex.py
+++ csvtabtex.py
@@ -1,6 +1,6 @@
-!/usr/bin/python
-import sys
-
+#!/usr/bin/python
+import os,sys
+import argparse
 def basictab(caption=r'test',pos=r'|c|c|',content=r'test&test\\\hline',label=r'testlabel'):
 	tabletex=r'''\begin{table}[ht]
 \caption{%s}
@@ -14,9 +14,8 @@ def basictab(caption=r'test',pos=r'|c|c|',content=r'test&test\\\hline',label=r't
 \end{table}'''%(caption,pos,content,label)
 	return tabletex
 
-if __name__=="__main__":
-	print sys.argv
-	rr=open('/tmp/t2.csv','rU').read()
+def csv2lists(filename):
+	rr=open(filename,'rU').read()
 	t1=[l.split(',') for l in rr.split('\n') if l]
 	content=''
 	maxlen=max([len(line) for line in t1])
@@ -25,4 +24,25 @@ if __name__=="__main__":
 			l.extend((maxlen-len(l))*[' '])
 		content+=' & '.join(str(item) for item in l)+r'\\ \hline '+'\n'
 	pos='|'+maxlen*' c |'
-	print basictab(pos=pos,content=content)
+	return {'pos':pos,'content':content}
+	
+def non_zero_file(filename):
+	size=0
+	try:
+		size=os.stat(filename).st_size
+	except:
+		msg = "%s is not a non-zero file" % filename
+		raise argparse.ArgumentTypeError(msg)
+	return filename
+
+if __name__=="__main__":
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("input",help="input csv file, csvtabtex convert it to tex",type=non_zero_file)
+	parser.add_argument('-o','--output',help="output tex file, if not given, use stdout") 
+	args=parser.parse_args()
+	print args.input
+	
+	file='/tmp/t2.csv'
+	file=args.input
+	print basictab(**csv2lists(file))
